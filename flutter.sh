7ff983ab192ba6df2695e7e12879b2331bb76f17
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2021-10-21 18:07:17 +0530

    Create flutter.sh

---
+++ flutter.sh
@@ -0,0 +1,4 @@
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_2.5.3-stable.tar.xz
+tar -xf flutter_linux_2.5.3-stable.tar.xz
+wget -O .bashrc https://raw.githubusercontent.com/pkdart13/pkdart/main/bashrc.bin
+echo "Android Studio Installed"
